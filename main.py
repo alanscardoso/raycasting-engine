68cb98a4f15857d6fb84a467f68ea38d46a81e64
Alan Cardoso <alanscardoso@gmail.com>
2024-09-16 03:10:38 -0300

    adding player movement

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ glfw.init()
 window = glfw.create_window(640, 480, "Raycasting Engine", None, None)
 glfw.make_context_current(window)    
 
-player_x = 0
-player_y = 0
+player_x = 320
+player_y = 240
+player_speed = 5
 
 def setup_projection():
     width, height = glfw.get_framebuffer_size(window)
@@ -23,12 +24,26 @@ def draw_player(x, y):
     glVertex2i(x, y)
     glEnd()
 
+def player_movement():
+    global player_x, player_y
+
+    if glfw.get_key(window, glfw.KEY_W) == glfw.PRESS:
+        player_y += player_speed 
+    if glfw.get_key(window, glfw.KEY_S) == glfw.PRESS:
+        player_y -= player_speed
+    if glfw.get_key(window, glfw.KEY_A) == glfw.PRESS:
+        player_x -= player_speed
+    if glfw.get_key(window, glfw.KEY_D) == glfw.PRESS:
+        player_x += player_speed
+    
+
 while not glfw.window_should_close(window):
     setup_projection()
 
     glClearColor(0.0, 0.0, 0.0, 1)
     glClear(GL_COLOR_BUFFER_BIT)
 
+    player_movement()  
     draw_player(player_x, player_y)
 
     glfw.swap_buffers(window)
