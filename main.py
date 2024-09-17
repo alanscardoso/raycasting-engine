450e4c0d38b971078e660925d24e5fb91bfca3cb
Alan Cardoso <alanscardoso@gmail.com>
2024-09-17 03:20:57 -0300

    adding wall collision

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from OpenGL.GL import *
 
 width = 512
 height = 512
+# TODO we must ensure that the player will start in a space and not on a wall
 player_x = 256
 player_y = 256
 player_speed = 2
@@ -13,23 +14,52 @@ window = glfw.create_window(width, height, "Raycasting Engine", None, None)
 glfw.make_context_current(window)    
 
 
-def draw_2d_map():
-    square_size = 64
+# TODO extract map information to other file and load into here
+square_size = 64
+map_side = 8 # in squares, all maps are squareXsquare size
+map_2d = [
+    1,1,1,1,1,1,1,1,
+    1,0,0,0,0,0,1,1,
+    1,0,1,1,0,0,0,1,
+    1,0,1,0,0,1,0,1,
+    1,0,1,0,0,1,0,1,
+    1,0,0,0,1,1,0,1,
+    1,1,0,0,0,0,0,1,
+    1,1,1,1,1,1,1,1,
+]
+
+def is_wall_collision():
+    global map_2d, square_size, map_side, player_x, player_y
+
     x, y = 0, square_size*7
 
-    map2d = [
-        1,1,1,1,1,1,1,1,
-        1,0,0,0,0,0,0,1,
-        1,0,1,0,0,0,0,1,
-        1,0,1,1,0,1,0,1,
-        1,0,1,0,1,1,0,1,
-        1,0,0,0,0,1,0,1,
-        1,0,0,0,0,0,0,1,
-        1,1,1,1,1,1,1,1,
-    ]
+    count = 0
+    for i in map_2d:
+        vertex_0 = (x, y)
+        vertex_2 = (x + square_size, y + square_size) 
+
+        if player_x >= vertex_0[0] and player_x <= vertex_2[0]:
+            if player_y >= vertex_0[1] and player_y <= vertex_2[1]:
+                if i == 1:
+                    return True
+
+        x += square_size
+        count += 1
+
+        if count == map_side:
+            count = 0
+            y -= square_size
+            x = 0
+
+    return False
+
+# TODO must be a best way of drawing the map
+def draw_2d_map():
+    global map_2d, square_size, map_side
+    x, y = 0, square_size*7
 
     count = 0
-    for i in map2d:
+    for i in map_2d:
         if i == 0: 
             glColor3f(0,0,0)
         if i == 1:
@@ -46,7 +76,7 @@ def draw_2d_map():
         x += square_size
         count += 1
 
-        if count == 8:
+        if count == map_side:
             count = 0
             y -= square_size
             x = 0
@@ -55,17 +85,20 @@ def draw_2d_map():
 # change the center relative to a pixel base system to draw stuff
 def setup_projection():
     width, height = glfw.get_framebuffer_size(window)
+
     glViewport(0, 0, width, height)
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
     glOrtho(0, width, 0, height, -1, 1)
     glMatrixMode(GL_MODELVIEW)
 
-def draw_player(x, y):
+def draw_player():
+    global player_x, player_y
+
     glPointSize(8)
     glColor3f(1, 1, 0)
     glBegin(GL_POINTS)
-    glVertex2i(x, y)
+    glVertex2i(player_x, player_y)
     glEnd()
 
 def player_movement():
@@ -74,12 +107,23 @@ def player_movement():
 
     if glfw.get_key(window, glfw.KEY_W) == glfw.PRESS:
         player_y += player_speed 
+        if is_wall_collision():
+            player_y -= player_speed
+
     if glfw.get_key(window, glfw.KEY_S) == glfw.PRESS:
         player_y -= player_speed
+        if is_wall_collision():
+            player_y += player_speed
+
     if glfw.get_key(window, glfw.KEY_A) == glfw.PRESS:
         player_x -= player_speed
+        if is_wall_collision():
+            player_x += player_speed
+
     if glfw.get_key(window, glfw.KEY_D) == glfw.PRESS:
         player_x += player_speed
+        if is_wall_collision():
+            player_x -= player_speed
 
     if player_x < 0: 
         player_x = 0
@@ -100,7 +144,7 @@ while not glfw.window_should_close(window):
     draw_2d_map()
 
     player_movement()  
-    draw_player(player_x, player_y)
+    draw_player()
 
     glfw.swap_buffers(window)
     glfw.poll_events()
